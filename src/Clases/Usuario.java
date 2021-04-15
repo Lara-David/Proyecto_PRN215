e14cabc1941dff97ab63d83df2f2396225d12367
luisd <luisd@DESKTOP-EFH9FCU>
2021-04-14 21:22:15 -0600

    Formulario Sesion - Clase Usuario

---
+++ src/Clases/Usuario.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author luisd
+ */
+public class Usuario {
+    //Atributos
+    String nombres, apellidos, idUsuario, contrasenia;
+    int edad, telefono, numRegistro;
+    
+    //Metodo
+    public static void main(String [] args){
+        Usuario usuario1 = new Usuario();
+        
+    
+    
+        
+    }
+    
+}
